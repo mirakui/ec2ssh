21a5581fd4863f879f020a10ea6dac30242ef0e8
Issei Naruta <naruta@cookpad.com>
2013-12-05 22:21:52 +0900

    Separate ssh_config as a method

--- lib/ec2ssh/command/init.rb
+++ lib/ec2ssh/command/init.rb
@@ -4,21 +4,23 @@ require 'ec2ssh/ssh_config'
 module Ec2ssh
   module Command
     class Init < Base
-
       def initialize(cli)
         super
       end
 
       def run
-        config = SshConfig.new(ssh_config_path)
-        if config.mark_exist?
+        if ssh_config.mark_exist?
           cli.red "Marker already exists on #{ssh_config_path}"
         else
-          config.append_mark!
+          ssh_config.append_mark!
           cli.green "Added mark to #{ssh_config_path}"
         end
         #dotfile = Dotfile.update_or_create(options.dotfile, 'path' => config_path)
         cli.yellow "Please check and edit #{dotfile_path} before run `ec2ssh update`"
+      end
+
+      def ssh_config
+        @ssh_config ||= SshConfig.new(ssh_config_path)
       end
     end
   end
