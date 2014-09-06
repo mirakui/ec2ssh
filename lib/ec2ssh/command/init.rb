783b51e6026c3287797ed910bdcf262d8c807567
Issei Naruta <naruta@cookpad.com>
2014-09-06 11:01:58 +0900

    Implement init_dotfile

--- lib/ec2ssh/command/init.rb
+++ lib/ec2ssh/command/init.rb
@@ -10,13 +10,40 @@ module Ec2ssh
       end
 
       def run
-        init_dotfile
-        init_ssh_config
+        begin
+          init_ssh_config
+        rescue DotfileNotFound
+          init_dotfile
+          retry
+        end
       end
 
       def init_dotfile
-        #dotfile = Dotfile.update_or_create(options.dotfile, 'path' => config_path)
-        #cli.yellow "Please check and edit #{dotfile_path} before run `ec2ssh update`"
+        if File.exist?(dotfile_path)
+          cli.yellow "Warning: #{dotfile_path} already exists."
+          return
+        end
+
+        example = <<-DOTFILE
+path '#{ENV['HOME']}/.ssh/config'
+aws_keys(
+  default: {
+    access_key_id: ENV['AWS_ACCESS_KEY_ID'],
+    secret_access_key: ENV['AWS_SECRET_ACCESS_KEY']
+  },
+  # my_key1: { access_key_id: '...', secret_access_key: '...' }, ...
+)
+regions 'us-east-1'
+#reject {|instance| instance.tags['Name'] =~ /.../ }
+host_line <<END
+Host <%= tags['Name'] %>
+  HostName <%= private_ip_address %>
+END
+        DOTFILE
+
+        File.open(dotfile_path, 'w') {|f| f.write example }
+        cli.green "Generated #{dotfile_path}"
+        cli.yellow "Please check and edit #{dotfile_path} before run `ec2ssh update`"
       end
 
       def init_ssh_config
