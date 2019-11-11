ab0dcfab6c8b5eed57f6d075013a8ba1d292af57
Yuji Hanamura <yuji.developer@gmail.com>
2019-11-12 00:57:25 +0900

    Modify to abort if aws_keys called with old structure

--- spec/lib/ec2ssh/dsl_spec.rb
+++ spec/lib/ec2ssh/dsl_spec.rb
@@ -27,10 +27,9 @@ END
     let(:dsl_str) do
 <<-END
 aws_keys(
-  key1: { access_key_id: 'ACCESS_KEY1', secret_access_key: 'SECRET1' },
-  key2: { access_key_id: 'ACCESS_KEY2', secret_access_key: 'SECRET2' }
+  'key1' => { 'ap-northeast-1' => Aws::Credentials.new('ACCESS_KEY1', 'SECRET1') },
+  'key2' => { 'us-east-1' => Aws::Credentials.new('ACCESS_KEY2', 'SECRET2') }
 )
-regions 'ap-northeast-1', 'us-east-1'
 host_line 'host lines'
 reject {|instance| instance }
 path 'path'
@@ -40,13 +39,12 @@ END
     subject(:result) { Ec2ssh::Dsl::Parser.parse dsl_str }
 
     its(:profiles) { should be_nil }
-    its(:aws_keys) do
-      should == {
-        key1: { access_key_id: 'ACCESS_KEY1', secret_access_key: 'SECRET1' },
-        key2: { access_key_id: 'ACCESS_KEY2', secret_access_key: 'SECRET2' }
-      }
+    it do
+      expect(result.aws_keys).to match(
+        'key1' => { 'ap-northeast-1' => be_a(Aws::Credentials).and(have_attributes(access_key_id: 'ACCESS_KEY1', secret_access_key: 'SECRET1')) } ,
+        'key2' => { 'us-east-1' => be_a(Aws::Credentials).and(have_attributes(access_key_id: 'ACCESS_KEY2', secret_access_key: 'SECRET2')) }
+      )
     end
-    its(:regions) { should == ['ap-northeast-1', 'us-east-1'] }
     its(:host_line) { should == 'host lines' }
     it { expect(result.reject.call(123)).to eq(123) }
     its(:path) { should == 'path' }
@@ -56,8 +54,8 @@ END
     let(:dsl_str) do
 <<-END
 aws_keys(
-  key1: { access_key_id: 'ACCESS_KEY1', secret_access_key: 'SECRET1' },
-  key2: { access_key_id: 'ACCESS_KEY2', secret_access_key: 'SECRET2' }
+  'key1' => { 'ap-northeast-1' => Aws::Credentials.new('ACCESS_KEY1', 'SECRET1') },
+  'key2' => { 'us-east-1' => Aws::Credentials.new('ACCESS_KEY2', 'SECRET2') }
 )
 profiles 'default', 'myprofile'
 regions 'ap-northeast-1', 'us-east-1'
@@ -70,5 +68,22 @@ END
     it do
       expect { Ec2ssh::Dsl::Parser.parse dsl_str }.to raise_error Ec2ssh::DotfileValidationError
     end
+  end
+
+  context 'with old structure aws_keys' do
+    let(:dsl_str) do
+<<-END
+aws_keys(
+  key1: { access_key_id: 'ACCESS_KEY1', secret_access_key: 'SECRET1' },
+  key2: { access_key_id: 'ACCESS_KEY2', secret_access_key: 'SECRET2' }
+)
+regions 'ap-northeast-1', 'us-east-1'
+host_line 'host lines'
+reject {|instance| instance }
+path 'path'
+END
+    end
+
+    it { expect { Ec2ssh::Dsl::Parser.parse dsl_str }.to output("aws_keys structure is changed. Please change your .ec2ssh syntax.\n").to_stderr.and(raise_error(SystemExit)) }
   end
 end
